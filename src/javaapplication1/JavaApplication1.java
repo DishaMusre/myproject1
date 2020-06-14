34e2059d87ef1f873ddc83d7c91627ef216d9e1e
Attiq <dishamusre48@gmail.com>
2020-06-14 14:09:32 +0530

    My very first commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
+import java.sql.*;
+import java.util.logging.Logger;
+
+//package javaapplication1;
+
+
+/**
+ *
+ * @author MUSRE DISHA
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+     
+
+    }
+    
+}
+
+
+
